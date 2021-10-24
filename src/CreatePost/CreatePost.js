f51c670ca04ec569f412b329696e72699d8b7ffa
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2021-10-24 19:21:52 +0530

    Update CreatePost.js

--- src/CreatePost/CreatePost.js
+++ src/CreatePost/CreatePost.js
@@ -40,7 +40,7 @@ const CreatePost = () => {
   return (
     <div className={`py-2 ${classes.main}`}>
       <img src={p1} alt='p1'/>
-        <form className={`col-8 col-sm-4 col-lg-3 mx-2 p-4 rounded ${classes.body}`}>
+        <form className={`col-8 col-sm-4 col-lg-3 mx-2 p-4 rounded ${classes.body}`} onSubmit={submithandler}>
           <label> User </label>
           <select className="form-control form-control-sm form-select mb-2"value={state.userId}onChange={changehandle} name="userId">
             {users.map((ele) => (<option key={ele.id} value={ele.id}>{ele.name}</option>))}
@@ -50,7 +50,7 @@ const CreatePost = () => {
           <label> Body </label>
           <input className="form-control form-control-sm  " name="body" value={state.body}onChange={changehandle} required />
           <br/>
-          <button className=" btn btn-primary" onClick={submithandler}>Submit</button>
+          <button className=" btn btn-primary" >Submit</button>
         </form>
         {state.flag && <Redirect to='/posts'/>}
     </div>
